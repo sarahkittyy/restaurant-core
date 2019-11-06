f0196107e73a9c4ae1ac08d0acbfaf11e8be8906
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-06 11:51:14 -0500

    implemeted api homepage

---
+++ resources/views/api-home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Restaurant-Core API v1.0.0</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+		<style>
+			html {
+				font-family: 'Nunito', sans-serif;
+			}
+			.text {
+				font-size: 50px;
+				text-decoration: underline;
+				padding: 5px;
+			}
+			.center {
+				align-items: center;
+				display: flex;
+				justify-content: center;
+			}
+		</style>
+	<head>
+		
+	<body>
+		<div class="center">
+			<p class="text">API v1.0.0</p>
+		</div>
+	</body>
+</html>
