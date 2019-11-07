b0fd3e2507acffc6d1188ec634d9e9843cfdb749
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-07 12:16:39 -0500

    /api/restaurants endpoint to upload new restaurants

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Restaurant;
 use Illuminate\Http\Request;
 
 /*
@@ -19,4 +20,48 @@ use Illuminate\Http\Request;
 
 Route::get("/", function (Request $request) {
 	return view('api-home');
+});
+
+/**
+ * @brief Upload endpoint for posting a new restaurant listing
+ * 
+ * POST PARAMS:
+ * - name -> string > len 3
+ * - address & image url
+ */
+Route::post('/restaurants', function (Request $request) {
+	/**
+	 * name -> required, len > 3
+	 * address -> required
+	 * image -> required
+	 */
+	$validator = Validator::make($request->all(), [
+		'name' => 'required|min:3',
+		'address' => 'required',
+		'image' => 'required'
+	]);
+
+	if ($validator->fails()) {
+		return redirect()
+			->route('/failure', ['msg' => 'Failed to upload new restaurant.'])
+			->withErrors($validator);
+	}
+	
+	// Creates and saves the restaurant model.
+	$restaurant = new Restaurant;
+	$restaurant->name = $request->name;
+	$restaurant->address = $request->address;
+	$restaurant->image = $request->image;
+	$restaurant->save();
+	
+	return response('Success!');
+});
+
+/**
+ * @brief Will retrieve a list of all restaurants in the database.
+ * 
+ * TODO: url parameters for sorting the output
+ */
+Route::get("/restaurants", function (Request $request) {
+	return '404';
 });
