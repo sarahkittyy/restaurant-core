28599d7b2ae6dfa4cc66f08bdf55ceeedf56c545
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-08 10:09:03 -0500

    added generic failure page

---
+++ resources/views/failure.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Restaurant-Core API v1.0.0</title>
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<style>
+		html {
+			font-family: 'Nunito', sans-serif;
+		}
+		.text {
+			font-size: 50px;
+			text-decoration: underline;
+			padding: 5px;
+			width: 100%;
+			margin: auto;
+		}
+		.center {
+			padding: 70px 0;
+			text-align: center;
+		}
+		.message {
+			font-size: 30px;
+		}
+		.return-link {
+			font-size: 30px;
+		}
+	</style>
+</head>
+<body>
+	<div class="center">
+		<p class="text">
+			Error {{ ':(' }}
+		</p>
+		<p class="message">
+			{{ $msg }}
+		</p>
+		<a class="return-link" href="/">Go Home</a>
+	</div>
+</body>
+</html>
