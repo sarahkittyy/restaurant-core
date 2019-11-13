2350a1fd4b26440018746e4a7369d032f760639e
Sarah Ohlin <sarahohlin1@gmail.com>
2019-11-13 09:08:48 -0500

    for implementing forms in blade templates

--- config/app.php
+++ config/app.php
@@ -160,7 +160,9 @@ return [
         Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
+		Illuminate\View\ViewServiceProvider::class,
+		
+		Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -224,8 +226,9 @@ return [
         'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-
+		'View' => Illuminate\Support\Facades\View::class,
+		'Form' => Collective\Html\FormFacade::class,
+		'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
